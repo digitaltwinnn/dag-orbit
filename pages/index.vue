d7b225193a0bb35aa14e089cd88a41e59c4e591e
digitaltwin <45341252+digitaltwinnn@users.noreply.github.com>
2022-11-22 12:34:15 +0100

    gsap to move the globe position

--- pages/index.vue
+++ pages/index.vue
@@ -43,6 +43,15 @@ export default {
             }
         });
 
+        gsap.to(this.$refs.threejs.naturalGlobe.get().position, {
+            x: 200, y: 200, z: 200,
+            scrollTrigger: {
+                trigger: ".panel-1",
+                scrub: 0.6,
+                markers: true
+            }
+        });
+
         gsap.to(".test2", {
             scrollTrigger: {
                 trigger: ".panel-2",
