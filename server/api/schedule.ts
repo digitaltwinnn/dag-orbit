6af8d28b9621d169ca83da27e1329c35c69d760c
digitaltwin <45341252+digitaltwinnn@users.noreply.github.com>
2022-11-27 10:15:34 +0100

    external scheduler instead of node-cron

--- server/api/schedule.ts
+++ server/api/schedule.ts
@@ -1,27 +1,27 @@
 import axios from "axios";
-import cron from "node-cron";
 import nodeModel from "../models/node.model";
 
-export default defineNitroPlugin((nitroApp) => {
-  cron.schedule("*/15 * * * *", () => {
-    nodeModel.find(function (err, docs) {
-      if (err) {
-        console.error(err);
-      } else {
-        console.info("mongodb nodes found: " + docs.length);
-        let ip = ipToNumber("54.215.18.98");
+export default defineEventHandler((event) => {
+  nodeModel.find(function (err, docs) {
+    if (err) {
+      console.error(err);
+    } else {
+      console.info("mongodb nodes found: " + docs.length);
+      let ip = ipToNumber("54.215.18.98");
 
-        // select random ip if more node are online
-        if (docs.length > 0) {
-          const doc = docs.at(randomNumber(0, docs.length - 1));
-          ip = doc.ip;
-        }
-
-        queryNodes(numberToIP(ip));
+      // select random ip if more node are online
+      if (docs.length > 0) {
+        const doc = docs.at(randomNumber(0, docs.length - 1));
+        ip = doc.ip;
       }
-    });
+
+      queryNodes(numberToIP(ip));
+    }
   });
-  console.info("Cron: scheduled the tasks to update nodes in mongodb");
+
+  return {
+    result: "works",
+  };
 });
 
 function queryNodes(ip: string) {
