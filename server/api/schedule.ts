3f7883a965f85492c6e90ece529b39188706db35
digitaltwin <45341252+digitaltwinnn@users.noreply.github.com>
2022-12-02 18:00:20 +0100

    more debug logs for prod

--- server/api/schedule.ts
+++ server/api/schedule.ts
@@ -3,27 +3,31 @@ import axios from "axios";
 import nodeModel from "../models/node.model";
 
 export default defineEventHandler((event) => {
-  console.log("readyState: "+ mongoose.connection.readyState);
+  console.log("mongodb readyState: " + mongoose.connection.readyState);
 
   // look for recently active nodes in mongodb
+  /*
   nodeModel.find(function (err, docs) {
-    console.log(".find err: " + err);
     if (err) {
       console.error(err);
     } else {
+      
       console.info("mongodb nodes found: " + docs.length);
+      */
       let clusterIP = "54.215.18.98";
 
+      /*
       // select a node ip if recently active nodes are found
       if (docs.length > 0) {
         const doc = docs.at(randomNumber(0, docs.length - 1));
         clusterIP = numberToIP(doc.ip);
       }
+      */
 
       // call the node to retrieve and update the mongodb store
       updateNodeDb(clusterIP);
-    }
-  });
+//    }
+//  });
 
   return {
     executed: true,
@@ -32,10 +36,11 @@ export default defineEventHandler((event) => {
 
 function updateNodeDb(ip: string): void {
   // query the node for all nodes in the cluster
+  console.log("updateNodeDb using api ip: " + ip)
   axios
     .get("http://" + ip + ":9000/cluster/info")
     .then(function (response) {
-      console.log("queried the ip: " + ip);
+      console.log("got axios response")
       let nodes = [];
       let i: number;
 
@@ -53,7 +58,7 @@ function updateNodeDb(ip: string): void {
 
       nodeModel
         .bulkWrite(nodes)
-        .then(() => console.log("bulkwrite completed"))
+        .then(() => console.log("mongodb bulkwrite completed"))
         .catch((e) => {
           console.error("nodejs error: " + e.message);
         });
