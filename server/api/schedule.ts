3131f2fb7ce2a1eb416abf3fa346b73dda97f0da
digitaltwin <45341252+digitaltwinnn@users.noreply.github.com>
2022-12-08 15:20:35 +0100

    refactoring

--- server/api/schedule.ts
+++ server/api/schedule.ts
@@ -1,61 +1,57 @@
-import mongoose from "mongoose";
 import axios from "axios";
 import nodeModel from "../models/node.model";
+import { AnyBulkWriteOperation } from "mongodb";
 
-export default defineEventHandler((event) => {
-  connectionIsUp().then(() => {
-    nodeModel
-      .find()
-      .then((docs) => {
-        console.info("mongodb nodes found: " + docs.length);
+export default defineEventHandler(async (event) => {
+  let updates = 0;
+  let success = false;
 
-        // select a node ip if recently active nodes are found
-        let clusterIP = "54.215.18.98";
-        if (docs.length > 0) {
-          const doc = docs.at(randomNumber(0, docs.length - 1));
-          clusterIP = numberToIP(doc.ip);
-        }
+  try {
+    const docs = await nodeModel.find();
+    console.info("nodes found in mongodb: " + docs.length);
 
-        // call the node to retrieve and update the mongodb store
-        updateNodeDb(clusterIP);
-      })
-      .catch((err) => {
-        console.error(err);
-      });
-  });
+    // select a node ip if recently active nodes are found
+    let clusterIP = "54.215.18.98";
+    if (docs.length > 0) {
+      const doc = docs.at(randomNumber(0, docs.length - 1));
+      if (doc) {
+        clusterIP = numberToIP(doc.ip);
+      }
+    }
+    // call the node to retrieve and update the mongodb store
+    updates = await updateNodeDb(clusterIP);
+    success = true;
+  } catch (e) {
+    success = false;
+    console.error(e);
+  }
 
   return {
-    executed: true,
+    success: success,
+    updates: updates,
   };
 });
 
-function updateNodeDb(ip: string): void {
+async function updateNodeDb(ip: string): Promise<number> {
   // query the node for all nodes in the cluster
-  axios
-    .get("http://" + ip + ":9000/cluster/info")
-    .then(function (response) {
-      let nodes = [];
-      let i: number;
+  const response = await axios.get("http://" + ip + ":9000/cluster/info");
 
-      // update mongodb with the returned nodes
-      response.data.forEach((n) => {
-        let docOperation = {
-          updateOne: {
-            filter: { ip: ipToNumber(n.ip) },
-            update: { $set: { state: n.state } },
-            upsert: true,
-          },
-        };
-        nodes.push(docOperation);
-      });
+  // update mongodb with the returned nodes
+  let nodes: AnyBulkWriteOperation[] = [];
+  response.data.forEach((n: { ip: string; state: string }) => {
+    let docOperation = {
+      updateOne: {
+        filter: { ip: ipToNumber(n.ip) },
+        update: { $set: { state: n.state } },
+        upsert: true,
+      },
+    };
+    nodes.push(docOperation);
+  });
+  await nodeModel.bulkWrite(nodes);
 
-      nodeModel.bulkWrite(nodes).catch((e) => {
-        console.error("nodejs error: " + e.message);
-      });
-    })
-    .catch(function (e) {
-      console.error("axios error: ", e.message);
-    });
+  console.log("nodes refreshed in mongodb from cluster: " + nodes.length);
+  return nodes.length;
 }
 
 function randomNumber(min: number, max: number) {
@@ -82,16 +78,4 @@ function ipToNumber(ip: string) {
       return (ipNumber << 8) + parseInt(octet, 10);
     }, 0) >>> 0
   );
-}
-
-async function connectionIsUp(): Promise<boolean> {
-  try {
-    return await mongoose.connection.db
-      .admin()
-      .ping()
-      .then((res) => res?.ok === 1);
-  } catch (err) {
-    console.error(err);
-    return false;
-  }
 }
