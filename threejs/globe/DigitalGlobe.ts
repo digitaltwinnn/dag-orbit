92778ed6cf7abe3e974bb92494da5c561110bb4e
digitaltwin <45341252+digitaltwinnn@users.noreply.github.com>
2023-03-20 17:32:16 +0100

    refactoring

--- threejs/globe/DigitalGlobe.ts
+++ threejs/globe/DigitalGlobe.ts
@@ -9,7 +9,6 @@ import {
   MathUtils,
   MeshPhongMaterial,
   Object3D,
-  Vector2,
   Vector3,
 } from "three";
 import { GlobeUtils } from "../utils/GlobeUtils";
@@ -21,60 +20,54 @@ const shieldColors = ["#1E90FE", "#1467C8", "#1053AD"];
 
 class DigitalGlobe {
   private group!: Group;
-  private dots!: InstancedMesh;
+  private mesh!: InstancedMesh;
   private dotDensity = 0.5;
   private rows = 150;
   private radius = 130;
   private globeDots: Vector3[] = [];
   private mapDots: Vector3[] = [];
   private positions: Vector3[] = [];
-  private colorInterval = false;
   private mode = GLOBE;
 
   constructor(appScene: AppScene) {
     this.group = new Group();
 
-    // digital globe
     const $img = useImage();
     const imgUrl = $img("/earthspec1k.jpg", { width: 1024 });
     const loader = new ImageLoader();
     loader.load(imgUrl, (image) => {
-      const canvas = document.createElement("canvas");
-      canvas.width = image.width;
-      canvas.height = image.height;
+      this.generateDots(image);
 
-      const context = canvas.getContext("2d", { willReadFrequently: true });
-      if (context) {
-        context.drawImage(image, 0, 0);
-        this.mapDots = this.createMapDots(image, context);
-        this.globeDots = this.createGlobeDots(image, context);
+      this.mesh = this.createInstancedMesh(this.globeDots);
+      this.group.add(this.mesh);
+      this.animateColors();
 
-        if (this.mode == MAP) {
-          this.dots = this.createMesh(this.mapDots);
-        } else {
-          this.dots = this.createMesh(this.globeDots);
-        }
-        this.group.add(this.dots);
-        this.rotateColors();
-      }
+      appScene.add(this.group);
+      appScene.addObjectAnimation(this);
     });
-
-    appScene.add(this.group);
-    appScene.addObjectAnimation(this);
   }
 
-  private createMesh(positions: Vector3[]): InstancedMesh {
-    this.positions = [];
-    positions.forEach((val) => this.positions.push(Object.assign({}, val)));
+  private generateDots(image: HTMLImageElement) {
+    const canvas = document.createElement("canvas");
+    canvas.width = image.width;
+    canvas.height = image.height;
 
-    const baseGeometry = new CircleGeometry(0.7, 6);
-    const instancedGeometry = new InstancedBufferGeometry().copy(baseGeometry);
-    const material = new MeshPhongMaterial({
-      side: DoubleSide,
-      transparent: true,
-    });
+    const context = canvas.getContext("2d", { willReadFrequently: true });
+    if (context) {
+      context.drawImage(image, 0, 0);
+      this.mapDots = this.createMapDots(image, context);
+      this.globeDots = this.createGlobeDots(image, context);
+    }
+  }
+
+  private createInstancedMesh(dots: Vector3[]): InstancedMesh {
+    dots.forEach((dot) => this.positions.push(Object.assign({}, dot)));
+
+    const circle = new CircleGeometry(0.7, 6);
+    const instancedCircle = new InstancedBufferGeometry().copy(circle);
+    const material = new MeshPhongMaterial({ side: DoubleSide });
     const mesh = new InstancedMesh(
-      instancedGeometry,
+      instancedCircle,
       material,
       this.positions.length
     );
@@ -84,10 +77,10 @@ class DigitalGlobe {
     for (let i = 0; i < this.positions.length; i++) {
       const pos = this.positions[i];
       dummy.position.set(pos.x, pos.y, pos.z);
-      dummy.lookAt(0, 0, 0);
+      this.orientateMesh(dummy);
+
       dummy.updateMatrix();
       mesh.setMatrixAt(i, dummy.matrix);
-
       color.set(shieldColors[MathUtils.randInt(0, shieldColors.length - 1)]);
       mesh.setColorAt(i, color);
     }
@@ -95,35 +88,37 @@ class DigitalGlobe {
     return mesh;
   }
 
-  private rotateColors() {
-    if (!this.colorInterval) {
-      this.colorInterval = true;
-
-      setInterval(() => {
-        const color = new Color();
-
-        for (let i = 0; i < this.positions.length; i++) {
-          const random = Math.random();
-          if (random > 0.98) {
-            color.set(
-              shieldColors[MathUtils.randInt(0, shieldColors.length - 1)]
-            );
-            this.dots.setColorAt(i, color);
-          }
-        }
-
-        if (this.dots.instanceColor) {
-          this.dots.instanceColor.needsUpdate = true;
-        }
-      }, 100);
+  private orientateMesh(dummy: Object3D) {
+    if (this.mode == GLOBE) {
+      dummy.lookAt(0, 0, 0);
+    } else {
+      dummy.rotation.set(0, 0, Math.PI / 2);
     }
+  }
+
+  private animateColors() {
+    const color = new Color();
+    setInterval(() => {
+      for (let i = 0; i < this.positions.length; i++) {
+        if (Math.random() > 0.98) {
+          color.set(
+            shieldColors[MathUtils.randInt(0, shieldColors.length - 1)]
+          );
+          this.mesh.setColorAt(i, color);
+        }
+      }
+
+      if (this.mesh.instanceColor) {
+        this.mesh.instanceColor.needsUpdate = true;
+      }
+    }, 100);
   }
 
   private createGlobeDots(
     image: HTMLImageElement,
     context: CanvasRenderingContext2D
   ): Vector3[] {
-    const positions = [];
+    const dots = [];
 
     for (let lat = -90; lat <= 90; lat += 180 / this.rows) {
       const r = Math.cos((Math.abs(lat) * Math.PI) / 180) * this.radius;
@@ -133,7 +128,7 @@ class DigitalGlobe {
       for (let x = 0; x < dotsForLat; x++) {
         const long = -180 + (x * 360) / dotsForLat;
 
-        const coordinate = this.latLongToXY(
+        const coordinate = GlobeUtils.latLongToXY(
           lat,
           long,
           image.width,
@@ -147,7 +142,7 @@ class DigitalGlobe {
         ).data;
 
         if (pixelData[0] <= 5) {
-          positions.push(
+          dots.push(
             GlobeUtils.toVector(lat, long, this.radius)
               .normalize()
               .multiplyScalar(this.radius)
@@ -155,59 +150,62 @@ class DigitalGlobe {
         }
       }
     }
-    return positions;
+    return dots;
   }
 
   private createMapDots(
     image: HTMLImageElement,
     context: CanvasRenderingContext2D
   ): Vector3[] {
-    const positions = [];
-
-    // to get close to same amount of dots when projected on 3d globe
-    const step = 4.8;
+    const dots = [];
+    const step = 4.2;
 
     for (let x = 0; x < image.width; x += step) {
       for (let y = 0; y < image.height; y += step) {
         const pixelData = context.getImageData(x, y, 1, 1).data;
-
         if (pixelData[0] <= 5) {
           const posX = x - 0.5 * image.width;
-          const posY = y - 0.5 * image.height;
-          positions.push(new Vector3(posX / 3, posY / 3, 0));
+          const posY = -y + 0.5 * image.height;
+          dots.push(new Vector3(posX / 3, posY / 3, 0));
         }
       }
     }
-    return positions;
+    return dots;
   }
 
-  private transformMesh(target: Vector3[]) {
-    if (target.length >= this.positions.length) {
+  private transformMesh(target: Vector3[], mode: number) {
+    this.mode = mode;
+
+    const delta = target.length - this.positions.length;
+    if (delta >= 0) {
+      // more positions in target, ignore the rest
       const dummy = new Object3D();
       for (let i = 0; i < this.positions.length; i++) {
         const newPos = target[i];
         dummy.position.set(newPos.x, newPos.y, newPos.z);
-        if (this.mode == GLOBE) {
-          dummy.lookAt(0, 0, 0);
-        }
+        this.orientateMesh(dummy);
         dummy.updateMatrix();
-        this.dots.setMatrixAt(i, dummy.matrix);
+        this.mesh.setMatrixAt(i, dummy.matrix);
       }
-      this.dots.instanceMatrix.needsUpdate = true;
+      this.mesh.instanceMatrix.needsUpdate = true;
     } else {
-      console.warn("couldn't transform mesh");
+      // less positions in target, fillers for the rest
+      const dummy = new Object3D();
+      for (let i = 0; i < target.length; i++) {
+        const newPos = target[i];
+        dummy.position.set(newPos.x, newPos.y, newPos.z);
+        this.orientateMesh(dummy);
+        dummy.updateMatrix();
+        this.mesh.setMatrixAt(i, dummy.matrix);
+      }
+      // fillers
+      for (let i = target.length; i < this.positions.length; i++) {
+        dummy.position.set(0, 0, 0);
+        dummy.updateMatrix();
+        this.mesh.setMatrixAt(i, dummy.matrix);
+      }
+      this.mesh.instanceMatrix.needsUpdate = true;
     }
-  }
-
-  private latLongToXY(
-    lat: number,
-    long: number,
-    mapWidth: number,
-    mapHeight: number
-  ): Vector2 {
-    const y = (-1 * lat + 90) * (mapHeight / 180);
-    const x = (long + 180) * (mapWidth / 360);
-    return new Vector2(Math.floor(x), Math.floor(y));
   }
 
   public get(): Group {
